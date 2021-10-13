Fixed off-by-one time limit error

gym_po/envs/car_flag.py
@@ -82,7 +82,7 @@ class CarVecEnv(gym.Env):
         rewards = np.zeros(self.num_envs, dtype=np.float32)
         rewards[(hh == self.heavens) & dones] = 1.
         rewards[(hh == self.hells) & dones] = -1.
-        dones |= (self.elapsed > self.time_limit)
+        dones |= (self.elapsed >= self.time_limit)
         directions = np.where((new_position >= self.PRIEST - self.PRIEST_THRESHOLD) &
                                (new_position <= self.PRIEST + self.PRIEST_THRESHOLD), 1., 0.)
         directions[directions == 1] = self.heavens[directions == 1]

gym_po/envs/taxi.py
@@ -288,7 +288,7 @@ class TaxiVecEnv(Env):
         rew[bad_move] = self.BAD_MOVE
         done = np.zeros(self.num_envs, bool)
         done[goal_move] = True
-        done[self.elapsed > self.time_limit] = True
+        done[self.elapsed >= self.time_limit] = True
         self._reset_mask(done)
         return self._obs(), rew, done, [{}] * self.num_envs
 