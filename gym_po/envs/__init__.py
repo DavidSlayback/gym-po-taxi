fcfd28c31b942a2da0dbc6207178e50dfb4cd2c8
David Slayback <slayback.d@northeastern.edu>
2022-02-03 10:43:50 -0500

    Export regular taxi map

--- gym_po/envs/__init__.py
+++ gym_po/envs/__init__.py
@@ -1,5 +1,5 @@
 # from .taxi_v2 import TaxiEnv, TaxiVecEnv, HansenTaxiVecEnv
-from .extended_taxi import TaxiVecEnv, HansenTaxiVecEnv, ExtendedHansenTaxiVecEnv, EXTENDED_TAXI_MAP, ExtendedTaxiVecEnv
+from .extended_taxi import TaxiVecEnv, HansenTaxiVecEnv, ExtendedHansenTaxiVecEnv, EXTENDED_TAXI_MAP, ExtendedTaxiVecEnv, TAXI_MAP
 from .car_flag import CarVecEnv, DiscreteActionCarVecEnv
 from .fourrooms import FourRoomsVecEnv
 from .multistory_fourrooms import MultistoryFourRoomsVecEnv, HansenMultistoryFourRoomsVecEnv, GridMultistoryFourRoomsVecEnv
