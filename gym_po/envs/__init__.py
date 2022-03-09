cbff104fa44a8294820345d86ccd05b88fd860bd
7thStringofZhef <68602513+DavidSlayback@users.noreply.github.com>
2022-03-09 17:10:12 -0500

    More time

--- gym_po/envs/__init__.py
+++ gym_po/envs/__init__.py
@@ -15,11 +15,11 @@ from gym.envs.registration import register
 register(
     id='pdomains-ant-heaven-hell-v1',
     entry_point='gym_po.envs.ant_heaven_hell:AntHeavenHellEnv',
-    max_episode_steps=400,
+    max_episode_steps=500,
 )
 
 register(
     id='pdomains-ant-tag-v1',
     entry_point='gym_po.envs.ant_tag:AntTagEnv',
-    max_episode_steps=400,
+    max_episode_steps=500,
 )
